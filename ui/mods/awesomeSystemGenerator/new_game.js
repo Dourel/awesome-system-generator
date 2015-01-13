d88b9a57af34e0925309cd2ce3e1a6ad1339a44a
Ray Speth <speth@mit.edu>
2015-01-12 23:31:23 -0500

    Assign planets to random, structured orbits
    Generate more compact planetary systems by allowing multiple planets per
    orbit and by allowing planets to have moons. Planets are allowed to orbit
    other planets based on their size. Selection of orbit is based on weighted
    preferences for various orbit types. Orbital distances are calculated to
    avoid collisions and mininterpretation by the game's sometimes bizarre
    orbital mechanics rules.

--- ui/mods/awesomeSystemGenerator/new_game.js
+++ ui/mods/awesomeSystemGenerator/new_game.js
@@ -16,9 +16,17 @@ model.loadRandomSystem = function() {
 
 var generateSystem = function(config) {
     var rng = new Math.seedrandom(config.seed !== undefined ? config.seed : Math.random());
+    //var rng = new Math.seedrandom(6);
     var getRandomInt = function (min, max) {
         return Math.floor(rng() * (max - min + 1)) + min;
     };
+
+    // Weighted random choice from an array, where each element in the array is
+    // the cumulative probability for the current and all previous elements
+    var getChoice = function(arr) {
+        var x = getRandomInt(1, arr[arr.length-1]-1);
+        return _.findLastIndex(arr, function (k) { return k < x; }) + 1;
+    }
 
     var rSystem = {
         name: 'Awesome System ' + getRandomInt(100, 30000),
@@ -30,7 +38,7 @@ var generateSystem = function(config) {
     var nMedium = parseInt($('input#medium-planets').val());
     var nSmall = parseInt($('input#small-planets').val());
     var nTiny = parseInt($('input#tiny-planets').val());
-    var nGas = parseInt($('input#gas-giants').val()); // adds an extra 'large'
+    var nGas = parseInt($('input#gas-giants').val());
     var nMetal = parseInt($('input#metal-planets').val()); // small, medium, or large
     var nStart = parseInt($('input#start-planets').val()); // small, medium, and large
     var nLaunch = parseInt($('input#launchable-planets').val()); // small and tiny
@@ -49,14 +57,13 @@ var generateSystem = function(config) {
     for (var i = 0; i < nLarge+nMedium+nSmall+nTiny-nMetal; i++) {
         specs.push({biome: ['earth', 'desert', 'lava', 'tropical']});
     }
-    console.log(specs);
     specs = _.shuffle(specs);
 
     // Populate sizes
     var sizes = [{n:nTiny, m:5000, r1:100, r2:200, bad:'metal'},
                  {n:nSmall, m:10000, r1:200, r2:300, bad:''},
                  {n:nMedium, m:20000, r1:300, r2:500, bad:''},
-                 {n:nLarge, m:50000, r1:500, r2:750, bad:''}];
+                 {n:nLarge, m:40000, r1:500, r2:750, bad:''}];
 
     for (var j = 0; j < sizes.length; j++) {
         var nLeft = sizes[j]['n'];
@@ -100,33 +107,203 @@ var generateSystem = function(config) {
         specs[i]['launch'] = [2, 4];
         nLeft -= 1;
     }
-    specs = _.shuffle(specs);
+
+    // Assign planets to orbits, in decreasing order by size
+    specs.reverse();
+    for (var i = 0; i < specs.length; i++) {
+        specs[i].id = i;
+    }
+    var orbits = [
+        {planet: -1, children: [], weight: 5, capacity: 4, maxMass: 1e5},
+        {planet: -1, children: [], weight: 3, capacity: 5, maxMass: 1e5},
+        {planet: -1, children: [], weight: 2, capacity: 5, maxMass: 1e5},
+        {planet: -1, children: [], weight: 1, capacity: 3, maxMass: 1e5}];
+
+    for (var i = 0; i < specs.length; i++) {
+        var spec = specs[i];
+        // Pick an orbit for this planet
+        var cdf = [];
+        var total = 0;
+        for (var j = 0; j < orbits.length; j++) {
+            var orbit = orbits[j];
+            if (spec.mass <= orbit.maxMass && orbit.capacity) {
+                total += orbit.weight;
+            }
+            cdf.push(total);
+        }
+        var orbit = orbits[getChoice(cdf)];
+        orbit.children.push(spec.id);
+        spec.orbit = orbit;
+        if (orbit.children.length == orbit.capacity) {
+            orbit.weight = 0;
+        }
+
+        // Add orbits provided by this planet, if any.
+        // Planets orbiting other planets may not provide orbits.
+        if (spec.mass >= 50000 && orbit.planet == -1) {
+            // Gas giant: permits medium and smaller as moons
+            orbits.push({planet: spec.id, children: [], weight: 8,
+                         capacity: 2, maxMass: 20000});
+            orbits.push({planet: spec.id, children: [], weight: 6,
+                         capacity: 2, maxMass: 20000});
+        } else if (spec.mass >= 40000 && orbit.planet == -1) {
+            // Large planet: permits small and tiny as moons
+            orbits.push({planet: spec.id, children: [], weight: 5,
+                         capacity: 2, maxMass: 10000});
+        } else if (spec.mass >= 20000 && orbit.planet == -1) {
+            // medium planet: permits tiny moons
+            orbits.push({planet: spec.id, children: [], weight: 4,
+                         capacity: 2, maxMass: 5000});
+        }
+    }
+    // Remove any empty orbits:
+    orbits = _.filter(orbits, function(orbit) { return orbit.children.length; });
+
+    // Stupid hack to prevent unintended capture: Set mass for all primary
+    // planets to be equal.
+    for (var j = 0; j < orbits.length; j++) {
+        var orbit = orbits[j];
+        if (orbit.planet != -1) {
+            break;
+        }
+        for (var i = 0; i < orbit.children.length; i++) {
+            specs[orbit.children[i]].mass = 50000;
+        }
+    }
+
+    // Link each orbit to its inner neighbor, if there is one
+    var lastPlanet = undefined;
+    var lastOrbit = undefined;
+    for (var j = 0; j < orbits.length; j++) {
+        if (lastPlanet == orbits[j].planet) {
+            orbits[j].inner = lastOrbit;
+        }
+        lastPlanet = orbits[j].planet;
+        lastOrbit = orbits[j];
+    }
+
+    // Set radius for orbits around planets
+    for (var j = 0; j < orbits.length; j++) {
+        var orbit = orbits[j];
+        if (orbit.planet == -1) {
+            continue;
+        }
+        var rmax = _.max(_.map(orbit.children,
+                               function(c) { return specs[c].radius}));
+        if (orbit.inner) {
+            orbit.radius = 2000 + orbit.inner.radius + rmax;
+        } else {
+            orbit.radius = Math.max(800 + 1.5*specs[orbit.planet].radius,
+                                       1600 + 3*rmax) + 1000;
+        }
+        specs[orbit.planet].rEffective = orbit.radius + rmax;
+    }
+
+    var getMaxRadius = function(ids) {
+        var rMax = 0;
+        for (var i = 0; i < ids.length; i++) {
+            if (specs[ids[i]].rEffective) {
+                rMax = Math.max(rMax, specs[ids[i]].rEffective);
+            } else {
+                rMax = Math.max(rMax, specs[ids[i]].radius);
+            }
+        }
+        return rMax;
+    }
+
+    // Determine radius of the first solar orbit
+    var getSolarMin = function(p) {
+        return (1e5*p.radius + 5e7)/p.mass + p.radius + 1000;
+    }
+    orbits[0].radius = 13000 + getMaxRadius(orbits[0].children);
+    for (var i = 0; i < orbits[0].children.length; i++) {
+        var planet = specs[orbits[0].children[i]];
+        var r = getSolarMin(planet);
+        // Loop over orbits around this planet and the planets in them as well
+        for (var j = 0; j < orbits.length; j++) {
+            if (orbits[j].planet != planet.id) {
+                continue;
+            }
+            for (var k = 0; k < orbits[j].children.length; k++) {
+                r = Math.max(r, orbits[j].radius +
+                                getSolarMin(specs[orbits[j].children[k]]));
+            }
+        }
+        orbits[0].radius = Math.max(orbits[0].radius, r);
+    }
+
+    // Set radii for remaining solar orbits
+    for (var j = 1; j < orbits.length; j++) {
+        var orbit = orbits[j];
+        if (orbit.planet != -1) {
+            break;
+        }
+
+        var rmax = Math.max(getMaxRadius(orbit.children),
+                            getMaxRadius(orbit.inner.children));
+        orbit.radius = orbit.inner.radius + 2 * rmax + 1000;
+    }
 
     var cSys = { Planets: []};
-    // Minimum allowable orbital radius
-    var r = (1e5*specs[0]['radius'] + 5e7)/specs[0]['mass'] + specs[0]['radius'] + 500;
-    r = _.max([r, 12500.0]);
-    for (var i = 0; i < specs.length; i++) {
-        r += 3000 + specs[i]['radius'];
+
+    //  Assign positions and velocities to all the planets; set up data used for
+    //  planet generation
+    for (var j = 0; j < orbits.length; j++) {
+        var orbit = orbits[j];
+        var N = orbit.children.length;
         var theta = getRandomInt(0, 360) / 360 * 2 * Math.PI;
-        var v = Math.sqrt(5e8 / r);
-        console.log(theta + ',' + r + ',' + v)
-        var p =  {
-                starting_planet: specs[i]['start'],
-                mass: specs[i]['mass'],
-                Thrust: specs[i]['launch'] || [0, 0],
-                Radius: [specs[i]['radius'], specs[i]['radius']],
+
+        // Orbital parameters of the body that this planet orbits
+        if (orbit.planet == -1) {
+            var r0 = [0,0];
+            var v0 = [0,0];
+            var M = 100000;
+        } else {
+            var parent = specs[orbit.planet];
+            var M = parent.mass;
+            var r0 = parent.position;
+            var v0 = parent.velocity;
+        }
+
+        for (var i = 0; i < N; i++) {
+            var child = specs[orbit.children[i]];
+            var r = orbit.radius;
+            var v = Math.sqrt(5000 * M / r);
+            child.position = [r0[0] + r * Math.cos(theta),
+                              r0[1] + r * Math.sin(theta)];
+            child.velocity = [v0[0] - v * Math.sin(theta),
+                              v0[1] + v * Math.cos(theta)];
+            theta += 2 * Math.PI / N;
+
+            var p =  {
+                starting_planet: child['start'],
+                mass: child['mass'],
+                Thrust: child['launch'] || [0, 0],
+                Radius: [child['radius'], child['radius']],
                 Height: [20, 25],
                 Water: [33, 35],
                 Temp: [0, 100],
                 MetalDensity: [25, 50],
                 MetalClusters: [0, 24],
                 BiomeScale: [100, 100],
-                Position: [r * Math.cos(theta), r * Math.sin(theta)],
-                Velocity: [v * Math.sin(theta), -v * Math.cos(theta)],
-                Biomes: specs[i]['biome']};
-        cSys['Planets'].push(p);
-        r += specs[i]['radius'];
+                Position: child['position'],
+                Velocity: child['velocity'],
+                Biomes: child['biome']};
+            cSys['Planets'].push(p);
+        }
+    }
+
+    // Debug printing
+    console.log('*****************************************');
+    for (var j = 0; j < orbits.length; j++) {
+        console.log('orbit', j, orbits[j].planet, orbits[j].radius,
+                    orbits[j].children);
+    }
+    console.log('---');
+    for (var j = 0; j < specs.length; j++) {
+        console.log('planet', j, specs[j].mass, specs[j].radius,
+            specs[j].position[0], specs[j].position[1],
+            specs[j].velocity[0], specs[j].velocity[1]);
     }
 
     var planet_template =
@@ -213,11 +390,11 @@ $(function () {
         table.append(tr);
     }
     addControl('large-planets', 'Large Planets', 0);
-    addControl('medium-planets', 'Medium Planets', 3);
+    addControl('medium-planets', 'Medium Planets', 2);
     addControl('small-planets', 'Small Planets', 0);
-    addControl('tiny-planets', 'Tiny Planets', 0);
-    addControl('gas-giants', 'Gas Giants', 0);
+    addControl('tiny-planets', 'Tiny Planets', 2);
+    addControl('gas-giants', 'Gas Giants', 1);
     addControl('metal-planets', 'Metal Planets', 0);
-    addControl('start-planets', 'Start Planets', 1);
-    addControl('launchable-planets', 'Launchable Planets', 0);
+    addControl('start-planets', 'Start Planets', 2);
+    addControl('launchable-planets', 'Launchable Planets', 2);
 });
